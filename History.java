1ca7e9a8de7cc57b27516f26050b71d680c51f59
winnieacer034-ops <winnieacer034@gmail.com>
2025-12-18 11:38:23 +0100

    Add History class for opera details management
    Cette classe  est utilise pour creer un historique des operations dans l'application

---
+++ History.java
@@ -0,0 +1,26 @@
+package com.javafx.Models;
+
+public class History {
+    private String operaName;
+    private String operaInfo;
+
+    public History(String operaName, String operaInfo){
+        this.operaName = operaName;
+        this.operaInfo = operaInfo;
+    }
+
+    public String getOperaName(){
+        return operaName;
+    }
+    public String getOperaInfo(){
+        return operaInfo;
+    }
+
+    public void setOperaName(String operaName){
+        this.operaName = operaName;
+    }
+    public void setOperInfo(String operaInfo){
+        this.operaInfo = operaInfo;
+    }
+
+}
